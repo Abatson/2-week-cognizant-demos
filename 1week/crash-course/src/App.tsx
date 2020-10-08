6b3df568b3c6d36dea5e00cf3d16f547b0714eb7
Abatson <abatson94@gmail.com>
2020-10-08 12:44:21 -0400

    added personalization and lifted state

--- 1week/crash-course/src/App.tsx
+++ 1week/crash-course/src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { CookieClicker } from './components/CookieClicker/CookieClicker';
+import { CookiePersonalization } from './components/CookiePersonalization/CookiePersonalization';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { NavBarComponent } from './components/NavBar/NavBar';
 
 function App() {
+  const [cookiePState, changeCookiePState] = useState({cookieName:'', storeName:'', userName:''})
+
   return (
     <div className="App">
-      <h1>Welcome to our Crash Course</h1>
-      <CookieClicker/>
-      <ToastContainer/>
+      <Router>
+        <NavBarComponent/>
+        <Switch>
+          <Route exact path='/' render={()=><CookieClicker cookiePState={cookiePState}/>} />
+          <Route path='/personalization' render={(props)=><CookiePersonalization {...props} cookiePState={cookiePState} changeCookiePState={changeCookiePState}/>} />
+        </Switch>
+        <ToastContainer />
+      </Router>
     </div>
   );
 }
--- 1week/crash-course/src/components/CookieClicker/CookieClicker.tsx
+++ 1week/crash-course/src/components/CookieClicker/CookieClicker.tsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Cookie from '../../assets/perfectCookie.png'
 
-function useInterval(callback:any, delay:any) {
+function useInterval(callback: any, delay: any) {
     const savedCallback = useRef<any>();
-  
+
     // Remember the latest callback.
     useEffect(() => {
-      savedCallback.current = callback;
+        savedCallback.current = callback;
     }, [callback]);
-  
+
     // Set up the interval.
     useEffect(() => {
-      function tick() {
-        savedCallback.current();
-      }
-      if (delay !== null) {
-        let id = setInterval(tick, delay);
-        return () => clearInterval(id);
-      }
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
     }, [delay]);
-  }
+}
 
 
 
@@ -41,8 +41,15 @@ const buildingCosts: any = {
     factories: 1000000
 }
 
+interface ICookieClickerProps {
+    cookiePState:{
+        cookieName:string
+        storeName:string
+        userName:string
+    }
+}
 
-export const CookieClicker: React.FunctionComponent<any> = (props) => {
+export const CookieClicker: React.FunctionComponent<ICookieClickerProps> = (props) => {
     const [cookies, changeCookies] = useState<number>(1000);
     const [cookiesPerSecond, changeCookiesPerSecond] = useState(0)
     const [buildingsMap, changeBuildingsMap] = useState({ grandmas: 0, bakeries: 0, farms: 0, mines: 0, factories: 0 })
@@ -72,37 +79,40 @@ export const CookieClicker: React.FunctionComponent<any> = (props) => {
 
 
     return (
-        <div className="cookie-clicker-container">
-            <figure>
-                <img onClick={() => changeCookies(cookies + 1)} src={Cookie} alt="A Chocolate Cookie made by grandma" height="150" width="150" />
-                <figcaption>You have {cookies.toFixed(0)} number of Cookies</figcaption>
-                <figcaption>You are making {cookiesPerSecond} cookies per second</figcaption>
-            </figure>
-            <div className="button-container">
-                <div>
-                    <p>Grandmas: {buildingsMap['grandmas']}</p>
-                    <p>Grandmas are Producing {buildingsMap['grandmas'] * cookieMultipliers['grandmas']} cookies per second</p>
-                    <button onClick={() => { buyBuilding('grandmas', 1) }}>Buy One</button>
-                </div>
-                <div>
-                    <p>Bakeries: {buildingsMap['bakeries']}</p>
-                    <p>bakeries are Producing {buildingsMap['bakeries'] * cookieMultipliers['bakeries']} cookies per second</p>
-                    <button onClick={() => { buyBuilding('bakeries', 1) }}>Buy One</button>
-                </div>
-                <div>
-                    <p>Farms: {buildingsMap['farms']}</p>
-                    <p>farms are Producing {buildingsMap['farms'] * cookieMultipliers['farms']} cookies per second</p>
-                    <button onClick={() => { buyBuilding('farms', 1) }}>Buy One</button>
-                </div>
-                <div>
-                    <p>Mines: {buildingsMap['mines']}</p>
-                    <p>mines are Producing {buildingsMap['mines'] * cookieMultipliers['mines']} cookies per second</p>
-                    <button onClick={() => { buyBuilding('mines', 1) }}>Buy One</button>
-                </div>
-                <div>
-                    <p>Factories: {buildingsMap['factories']}</p>
-                    <p>factories are Producing {buildingsMap['factories'] * cookieMultipliers['factories']} cookies per second</p>
-                    <button onClick={() => { buyBuilding('factories', 1) }}>Buy One</button>
+        <div>
+            <h1>Welcome to {props.cookiePState.storeName ? props.cookiePState.storeName : "Cookie Clicker"}</h1>
+            <div className="cookie-clicker-container">
+                <figure>
+                    <img onClick={() => changeCookies(cookies + 1)} src={Cookie} alt="A Chocolate Cookie made by grandma" height="150" width="150" />
+    <figcaption>You have {cookies.toFixed(0)} number of {props.cookiePState.cookieName} Cookies</figcaption>
+                    <figcaption>You are making {cookiesPerSecond} {props.cookiePState.cookieName} cookies per second</figcaption>
+                </figure>
+                <div className="button-container">
+                    <div>
+                        <p>Grandmas: {buildingsMap['grandmas']}</p>
+                        <p>Grandmas are Producing {buildingsMap['grandmas'] * cookieMultipliers['grandmas']} cookies per second</p>
+                        <button onClick={() => { buyBuilding('grandmas', 1) }}>Buy One</button>
+                    </div>
+                    <div>
+                        <p>Bakeries: {buildingsMap['bakeries']}</p>
+                        <p>bakeries are Producing {buildingsMap['bakeries'] * cookieMultipliers['bakeries']} cookies per second</p>
+                        <button onClick={() => { buyBuilding('bakeries', 1) }}>Buy One</button>
+                    </div>
+                    <div>
+                        <p>Farms: {buildingsMap['farms']}</p>
+                        <p>farms are Producing {buildingsMap['farms'] * cookieMultipliers['farms']} cookies per second</p>
+                        <button onClick={() => { buyBuilding('farms', 1) }}>Buy One</button>
+                    </div>
+                    <div>
+                        <p>Mines: {buildingsMap['mines']}</p>
+                        <p>mines are Producing {buildingsMap['mines'] * cookieMultipliers['mines']} cookies per second</p>
+                        <button onClick={() => { buyBuilding('mines', 1) }}>Buy One</button>
+                    </div>
+                    <div>
+                        <p>Factories: {buildingsMap['factories']}</p>
+                        <p>factories are Producing {buildingsMap['factories'] * cookieMultipliers['factories']} cookies per second</p>
+                        <button onClick={() => { buyBuilding('factories', 1) }}>Buy One</button>
+                    </div>
                 </div>
             </div>
         </div>
